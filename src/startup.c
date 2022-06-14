b6bb87fa5cc80eda2a92fe464820f6d6d18da1ec
rodrigoargondizo <rodrigo.argondizo@ufu.br>
2022-06-13 21:19:52 -0300

    att2

--- src/startup.c
+++ src/startup.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 
-#define SRAM_START 0x20000000U /* Inicio da SRAM CORTEX-M */
-#define SRAM_SIZE (128U * 1024U) /* Tam. SRAM STM32F411 128K */
+#define SRAM_START 0x20000000U  /* Inicio da SRAM CORTEX-M */
+#define SRAM_SIZE (64U * 1024U) /* Tam. SRAM STM32F401 64K */
 #define SRAM_END ((SRAM_START) + (SRAM_SIZE)) /* Final da SRAM STM32F411 */
 #define STACK_START SRAM_END /* Inicio da Stack */
 
